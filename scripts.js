2d08dc608e6710ff5742f2c52d64b4d031efb41f
Filipeelopess98 <filipee.lopess98@gmail.com>
2023-11-27 20:30:22 -0300

    Site - Ecommerce

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const menu = document.querySelector(".menu");
+const navMenu = document.querySelector(".nav-menu");
+
+menu.addEventListener("click", () => {
+    menu.classList.toggle('ativo');
+    navMenu.classList.toggle('ativo');
+})
+
+
+let formulario = document.querySelector(".fale")
+let mascara = document.querySelector(".form-mask")
+
+function cliqueBotao() {
+    formulario.style.left = "45px"
+    mascara.style.visibility = "visible"
+}
+
+function sairformulario() {
+    formulario.style.left = "-405px"
+    mascara.style.visibility = "hidden"
+}
